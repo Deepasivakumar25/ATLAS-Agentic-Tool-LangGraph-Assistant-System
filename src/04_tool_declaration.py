042f809340119b95d4a928784efab30be604f2c8
Deepasivakumar <103418256+Deepasivakumar25@users.noreply.github.com>
2026-09-03 13:26:20 +0530

    Create search tool declaration module

---
+++ src/04_tool_declaration.py
@@ -0,0 +1,15 @@
+"""Declare the Wikipedia and Google search tools."""
+
+from langchain_community.utilities import WikipediaAPIWrapper, GoogleSerperAPIWrapper
+from langchain_community.tools import WikipediaQueryRun, GoogleSerperRun
+
+
+api_wrapper = WikipediaAPIWrapper(
+    top_k_results=1,
+    doc_content_chars_max=300,
+)
+wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+
+
+google_api_wrapper = GoogleSerperAPIWrapper(k=1)
+google_search_tool = GoogleSerperRun(api_wrapper=google_api_wrapper)
