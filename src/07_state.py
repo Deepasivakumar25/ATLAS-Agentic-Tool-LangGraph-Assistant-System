cbadeceb994748cbff87e6247d54a25f0239c853
Deepasivakumar <103418256+Deepasivakumar25@users.noreply.github.com>
2026-09-03 13:36:40 +0530

    Remove old fragmented ATLAS source files

--- src/07_state.py
+++
@@ -1,9 +0,0 @@
-"""LangGraph state definition."""
-
-from typing import Annotated
-from typing_extensions import TypedDict
-from langgraph.graph.message import add_messages
-
-
-class State(TypedDict):
-    messages: Annotated[list, add_messages]
