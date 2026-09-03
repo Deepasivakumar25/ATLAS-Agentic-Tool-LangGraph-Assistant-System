385ba4e7e9650f9bc785901723cc59272d56f51b
Deepasivakumar <103418256+Deepasivakumar25@users.noreply.github.com>
2026-09-03 13:08:15 +0530

    Align state creation with agentic workflow

--- src/state.py
+++ src/state.py
@@ -3,5 +3,6 @@ from typing_extensions import TypedDict
 from langgraph.graph.message import add_messages
 
 
+# 3. State creation
 class State(TypedDict):
     messages: Annotated[list, add_messages]
