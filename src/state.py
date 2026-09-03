dc1f1dc0b2804244bdc02b8d8f044012597462df
Deepasivakumar <103418256+Deepasivakumar25@users.noreply.github.com>
2026-09-03 13:20:14 +0530

    Remove unnumbered files after notebook-order refactor

--- src/state.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated
-from typing_extensions import TypedDict
-from langgraph.graph.message import add_messages
-
-
-# 3. State creation
-class State(TypedDict):
-    messages: Annotated[list, add_messages]
